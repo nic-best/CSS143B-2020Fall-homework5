dbd52e969a599680d9ab885844ad3357a513eb52
Nicholas Bestauros <nicbest@uw.edu>
2020-11-12 19:27:23 -0800

    Initial Commit

--- src/main/java/Problem1/InOrderTraverse.java
+++ src/main/java/Problem1/InOrderTraverse.java
@@ -2,11 +2,14 @@ package Problem1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class InOrderTraverse {
     public static List<Integer> inorderTraversalIterative(TreeNode<Integer> root) {
-        // homework
+        Stack<TreeNode<Integer>> leftStack = new Stack<>();
+        Stack<TreeNode<Integer>> rootStack = new Stack<>();
+        Stack<TreeNode<Integer>> rightStack = new Stack<>();
         List<Integer> result = new ArrayList<>();
-        return result;  // place holder
+        return result;
     }
 }
