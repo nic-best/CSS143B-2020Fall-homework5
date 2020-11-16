c7171bde3c9faf2e72266c6ca44f74131a62e4b2
Nicholas Bestauros <nicbest@uw.edu>
2020-11-15 16:49:35 -0800

    Commented KDistance

--- src/main/java/ExtraCredit/KDistance.java
+++ src/main/java/ExtraCredit/KDistance.java
@@ -19,6 +19,10 @@ public class KDistance {
         return result;
     }
 
+    /*
+    Recursive method that traverses the entire TreeNode, creates a mirror treenode with the same structure
+    Sets the ancestor field in the ReversibleTreeNode to be the immediate ancestor of the new nodes. (the current node)
+     */
     private static ReversibleTreeNode<Integer> createReversible(TreeNode<Integer> root){
         ReversibleTreeNode<Integer> reverseRoot = new ReversibleTreeNode<Integer>(root.val);
         if(root.left!=null){
@@ -32,38 +36,56 @@ public class KDistance {
         return reverseRoot;
     }
 
+    /*
+    Recursive method that explores neighboring unvisited parts of the node (starting at any specified reversible treenode)
+    It increments a distance variable until we find the target node.
+    Will not backtrack because there is an unvisited list
+     */
     private static int findDistToTarget(ReversibleTreeNode<Integer> node, TreeNode<Integer> target, int dist, List<Integer> visited){
+        //visits the node
         visited.add(node.val);
+        //if this node is the target node, return our distance
         if(node.val.equals(target.val)){
             return dist;
         }
+        //if we havent visited this node yet
         if(node.left!=null && !visited.contains(node.left.val)){
             int distance = findDistToTarget(node.left, target, dist+1, visited);
+            //if this branch has the target node in it, return the distance
             if(distance!=-1){
                 return distance;
             }
         }
+        //if we havent visited this node yet
         if(node.right!=null && !visited.contains(node.right.val)){
             int distance = findDistToTarget(node.right, target, dist+1, visited);
+            //if this branch has the target node in it, return the distance
             if(distance!=-1){
                 return distance;
             }
         }
+        //if we havent visited this node yet
         if(node.ancestor!=null && !visited.contains(node.ancestor.val)){
             int distance = findDistToTarget(node.ancestor, target, dist+1, visited);
+            //if this branch has the target node in it, return the distance
             if(distance!=-1){
                 return distance;
             }
         }
+        //if we cant traverse any more nodes, and we havent found the target node, then this branch doesnt have the
+        //target node in it.
         return -1;
     }
 
     /*
-    sets the distance field for all treenodes in root tree
+    sets the distance field for all reversibletreenodes in the reversible distance tree
      */
     private static void setDistances(ReversibleTreeNode<Integer> root, TreeNode<Integer> target){
+        //list that stores the visited nodes (unique list for each instance of the recursive method)
         List<Integer> visited = new ArrayList<Integer>();
+        //get get the distance from the root to the target
         root.distToTarget = findDistToTarget(root, target, 0, visited);
+        //recursively get the distance from all the root's children and their children (etc) to the target
         if(root.left!=null){
             setDistances(root.left, target);
         }
@@ -72,6 +94,9 @@ public class KDistance {
         }
     }
 
+    /*
+    Traverses the ReversibleTree and saves all nodes with the specified distance to the list
+     */
     private static void getAllWithDistK(List<Integer> kList, ReversibleTreeNode<Integer> root, int k){
         if(root.distToTarget==k){
             kList.add(root.val);
@@ -84,6 +109,9 @@ public class KDistance {
         }
     }
 
+    /*
+    Stores a node's ancestor and has space to store the distance to the target node
+     */
     private static class ReversibleTreeNode<T> extends TreeNode<T> {
         public int distToTarget;
         public ReversibleTreeNode<T> ancestor;
